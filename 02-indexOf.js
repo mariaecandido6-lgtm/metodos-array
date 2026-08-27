2b8201c9a5615a09ca36a4b60c10535c8cd46737
mariaecandido6-lgtm <maria.e.candido6@edu.senai.br>
2026-08-27 09:27:42 -0300

    indexOf

---
+++ 02-indexOf.js
@@ -0,0 +1,25 @@
+//2. indexOf() - "Onde está?"
+//Procura a primeira posição do elemento. Retorna o índice (0, 1, 2...) ou -1 se não encontrar.
+
+console.log("=== 2. indexOf() ===");
+
+//Exemplo 1 - lista de frutas
+const frutas = ["Maçã", "Banana", "Laranja"];
+//índices: 0 -> Maçã, 1 -> Banana, 2 -> Laranja
+console.log("posição da banana:", frutas.indexOf("Banana")); //1
+
+//Exemplo 2 - procurando aluno
+const alunos = ["Ana", "João", "Carlos", "Mariana"];
+console.log("Posição de Mariana:", alunos.indexOf("Mariana")); //3
+
+//Exemplo 3 - quando não existe (retorna -1)
+console.log("Posição de Pedro:", alunos.indexOf("Pedro")); //1
+
+//Exemplo 4 - usando com if
+const posicao = alunos.indexOf("João");
+
+if (posicao !== -1) {
+    console.log("Aluno encontrado na posição:", posicao);
+} else {
+    console.log("Aluno não encontrado");
+}
